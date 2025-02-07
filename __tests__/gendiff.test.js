927998bb061b56db9910241c19fa113eb20c32ad
Dimitry Plehanov <mitya-plekhanov@mail.ru>
2025-02-07 17:19:57 +0300

    4 этап

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,11 +3,24 @@ import path from 'path';
 import fs from 'fs';
 
 const getFixturesPath = (filename) => path.resolve(process.cwd(), filename);
-const result = fs.readFileSync(getFixturesPath('result.txt'), 'utf-8');
-console.log('result', result);
+const result = fs.readFileSync(getFixturesPath('result.txt'), 'utf-8').trim();
+console.log('result:', result);
 test('gendiff', () => {
     expect(
         genddiff(getFixturesPath('file1.json'),getFixturesPath('file2.json'))
     )
     .toEqual(result);
 })
+
+
+
+// const getFixturesPath = (filename) => path.resolve(process.cwd(), filename);
+// const result = fs.readFileSync(getFixturesPath('result.txt'), 'utf-8');
+// console.log('result:', result);
+
+// test('gendiff', () => {
+//     const diff = genddiff(getFixturesPath('file1.json'), getFixturesPath('file2.json'));
+//     console.log('test:', diff); 
+
+//     expect(diff).toEqual(result);
+// });

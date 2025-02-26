ac2f21e3888cfaa3b0423c49ae1d1d586a60fb56
Dimitry Plehanov <mitya-plekhanov@mail.ru>
2025-02-26 20:21:34 +0300

    1

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/boyarkinnfdfdfdf/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/boyarkinnfdfdfdf/frontend-project-46/actions)
+
+
+# Проект "Вычислитель отличий"
+Вычислитель отличий – программа, определяющая разницу между двумя структурами данных. Это популярная задача, для решения которой существует множество онлайн сервисов, например http://www.jsondiff.com/. Подобный механизм используется при выводе тестов или при автоматическом отслеживании изменений в конфигурационных файлах.
+
+Возможности утилиты:
+
+- Поддержка разных входных форматов: yaml, json
+- Генерация отчета в виде plain text, stylish и json
+
+### Install
+Clone this repo: 
+```
+git clone https://github.com/boyarkinnfdfdfdf/frontend-project-46.git
+```
+
+Go to the project folder: 
+```
+cd frontend-project-46
+```
+
+Install package: 
+```
+make install
+npm link
+```
+
+### Usage
+To read help:
+```
+gendiff -h
+```
+
+To see version:
+```
+gendiff -V
+```
+
+To compare two files:
+```
+gendiff <path to file1> <path to file2>
+```
+
+To select the output format:
+```
+gendiff --format plain <path to file1> <path to file2>
+```
+
+### Examples
+#### Two JSON files with nested objects compare (stylish formatter)
+[![asciicast](https://asciinema.org/a/pc3BDqhr29nj5MeEKzyaGc9VO.svg)](https://asciinema.org/a/pc3BDqhr29nj5MeEKzyaGc9VO)
+
+#### Two YML files with nested objects compare (stylish formatter)
+[![asciicast](https://asciinema.org/a/KLcX4eh5E55uzYgYfbFn5AcCm.svg)](https://asciinema.org/a/KLcX4eh5E55uzYgYfbFn5AcCm)
+
+#### Plain formatter
+[![asciicast](https://asciinema.org/a/Q93Q2OpJN21sB2Sh2zk5AoGty.svg)](https://asciinema.org/a/Q93Q2OpJN21sB2Sh2zk5AoGty)
+
+#### JSON formatter
+[![asciicast](https://asciinema.org/a/qGGtupJFpdV3JNmKXKx3Op3oh.svg)](https://asciinema.org/a/qGGtupJFpdV3JNmKXKx3Op3oh)
--- src/index.js
+++ src/index.js
@@ -1,60 +1,24 @@
-import parser from './parser.js';
-import fs from 'fs';
-import path from 'path';
-import _ from 'lodash';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { cwd } from 'process';
+import parseFile from './parsers.js';
+import buildDiff from './buildDiff.js';
+import form from './formatters/formatters.js';
 
-
-export default (filepath1, filepath2) => {
-    const content1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
-    const content2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
-    // console.log('filepath1', fs.readFileSync(path.resolve(filepath1), 'utf-8'));
-    // console.log('filepath2', fs.readFileSync(path.resolve(filepath2), 'utf-8'));
-    const ext1 = path.extname(filepath1).substring(1); 
-    const ext2 = path.extname(filepath2).substring(1);
-    const parsedData1 = parser(content1, ext1);
-    const parsedData2 = parser(content2, ext2);
-    
-   const genDiff = (obj1, obj2) => {
-        const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])].sort();
-        const diff = keys.map((key) => {
-            if (!Object.hasOwn(obj2, key)) {
-                return `    - ${key}: ${obj1[key]}`;
-            }
-            if (!Object.hasOwn(obj1, key)) {
-                return `    + ${key}: ${obj2[key]}`;
-            }
-            if (obj1[key] !== obj2[key]) {
-                return `    - ${key}: ${obj1[key]}\n    + ${key}: ${obj2[key]}`;
-            }
-            return `      ${key}: ${obj1[key]}`;
-        });
-        return `{\n${diff.join('\n')}\n}`;
-    };
-     return genDiff(parsedData1, parsedData2);
+const readfile = (filepath) => {
+  const currentDir = cwd();
+  const absolutePath = resolve(currentDir, filepath);
+  const content = readFileSync(absolutePath, 'utf-8');
+  return content;
 };
 
+const getExtension = (file) => file.split('.')[1];
 
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  const file1 = parseFile(getExtension(filepath1), readfile(filepath1));
+  const file2 = parseFile(getExtension(filepath2), readfile(filepath2));
+  const diffTree = buildDiff(file1, file2);
+  return form(diffTree, format);
+};
 
-
-
-
-    // const buildTree = (obj1, obj2) => {
-    //     const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-
-    //     return keys.map((key) => {
-    //         if (!Object.hasOwn(obj2, key)) {
-    //             return { key, type: 'added', value: obj2[key] };
-    //         }
-    //         if (!Object.hasOwn(obj1, key)) {
-    //             return { key, type: 'removed', value: obj1[key] };
-    //         }
-    //         if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-    //             return { key, type: 'nested', children: buildTree(obj1[key], obj2[key]) };
-    //         }
-    //         if (!_.isEqual(obj1[key], obj2[key])) {
-    //             return { key, type: 'changed', oldValue: obj1[key], newValue: obj2[key] };
-    //         }
-    //         return { key, type: 'unchanged', value: obj1[key] };
-    //     });     
-    // };
-    // return buildTree(parsedData1, parsedData2);
+export default genDiff;
